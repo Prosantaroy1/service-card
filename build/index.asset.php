e9bc2086be841212eacf9c6304e6e48b295d763f
unknown <royprosanta172@gmail.com>
2025-10-07 19:05:40 +0600

    set to theme switch setting

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'd4dff5c8238cadb680f0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '855ff766313bdf05e099');
